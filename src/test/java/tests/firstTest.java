a4b2bede3b6275eadf76f3daa569ba2ab760b8a6
hp <hp@DESKTOP-3AKJFJI.lan>
2025-12-24 19:29:04 +0530

    This is forth commit

--- src/test/java/tests/firstTest.java
+++ src/test/java/tests/firstTest.java
@@ -1,40 +1,46 @@
 package tests;
 
 import java.io.IOException;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import Datautils.ExcelUtility;
 import pageObjects.home;
 import pageObjects.login;
+import reusable.Retry;
 import utils.Base;
 
 public class firstTest extends Base{
 	
-	@Test(dataProvider = "ExcelData")
+	@Test(dataProvider = "ExcelData", retryAnalyzer = Retry.class)
 	public void login(String userName, String password) throws InterruptedException {
 
 		Base.initBrowser("chrome");
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@id='mat-input-0']")).click();
-		System.out.println(userName);
-		driver.findElement(By.xpath("//input[@id='mat-input-0']")).sendKeys(userName);
-		driver.findElement(By.xpath("//input[@id='mat-input-1']")).sendKeys(password);
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//button[text()='Lets Go']")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//button[text()='Ok']")).click();
-		
-
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='mat-input-0']"))).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='mat-input-0']"))).sendKeys(userName);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='mat-input-1']"))).sendKeys(password);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Lets Go']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Ok']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@id='navbarDropdownProfile']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[@class='dropdown-item ng-star-inserted'])[4]"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()=' Yes ']"))).click();
+		driver.quit();
 	}
 	
 	@DataProvider(name = "ExcelData")
 	public Object[][] sendData() throws IOException {
 		return ExcelUtility.sendDatafromExcel("src/test/resources/data/LoginCredentials.xlsx", "Sheet1");
 	}
+	
+	
 }
